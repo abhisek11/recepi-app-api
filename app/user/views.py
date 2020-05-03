efb4aeb1ae6ab7f082260702f87fc8d9779cf197
Abhisek <abhisekrock94@gmail.com>
2020-05-03 10:55:08 +0530

    manage user update

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from django.db import transaction
@@ -50,3 +50,14 @@ class LoginView(KnoxLoginView):
                 return Response(data)
         except Exception as e:
             raise e
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication, )
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_object(self):
+        """Retrive and return authentication user"""
+        return self.request.user
